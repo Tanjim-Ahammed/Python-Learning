f791d805eb836fd7433cf03cd1514d1895515549
Tanjim-Ahammed <105290329+Tanjim-Ahammed@users.noreply.github.com>
2022-08-02 20:07:05 +0600

    Delete New-Market.py

--- New-Market.py
+++
@@ -1,6 +0,0 @@
-my_money = int(input("How much amount of money do you have?: "))
-rickshaw_bhara = int(input("How much amount of money need to go to NewMarket?: "))
-if my_money > rickshaw_bhara or my_money == rickshaw_bhara:
-    print("I can go to NewMarket")
-else:
-    print("I can't go to NewMArket")
