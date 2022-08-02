a2f4f52d78d2436c97d94deac0fc0087cf756851
Tanjim-Ahammed <105290329+Tanjim-Ahammed@users.noreply.github.com>
2022-08-02 20:06:55 +0600

    Delete Arithmetic.py

--- Arithmetic.py
+++
@@ -1,6 +0,0 @@
-from numpy import number
-
-
-number1 = int(input("Please enter a number: "))
-number2 = int(input("Please enter another number: "))
-print("number1 + number2 =", number1 + number2)     
